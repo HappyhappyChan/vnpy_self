982da608aa6e7c2b2b47dc14df7ec4b4593c4aa3
HappyhappyChan <1214999431@qq.com>
2022-10-26 17:24:13 +0800

    添加数据库和数据服务使用

--- examples/veighna_trader/run.py
+++ examples/veighna_trader/run.py
@@ -4,7 +4,19 @@ from vnpy.event import EventEngine
 from vnpy.trader.engine import MainEngine
 from vnpy.trader.ui import MainWindow, create_qapp
 
-# from vnpy_ctp import CtpGateway
+# 脚本加载数据库 --- begin 1.1--
+from datetime import datetime
+from typing import List
+from vnpy.trader.constant import Exchange, Interval
+from vnpy.trader.database import get_database
+from vnpy.trader.object import BarData, TickData
+# 脚本加载数据库 --- end 1.1--
+
+# 脚本加载TuShare数据服务 --- begin 2.1--
+from vnpy.trader.datafeed import get_datafeed
+from vnpy.trader.object import HistoryRequest
+
+from vnpy_ctp import CtpGateway
 # from vnpy_ctptest import CtptestGateway
 # from vnpy_mini import MiniGateway
 # from vnpy_femas import FemasGateway
@@ -50,7 +62,7 @@ def main():
 
     main_engine = MainEngine(event_engine)
 
-    # main_engine.add_gateway(CtpGateway)
+    main_engine.add_gateway(CtpGateway)
     # main_engine.add_gateway(CtptestGateway)
     # main_engine.add_gateway(MiniGateway)
     # main_engine.add_gateway(FemasGateway)
@@ -95,5 +107,60 @@ def main():
     qapp.exec()
 
 
+    # 脚本加载数据库 --- begin 1.2 --
+    database = get_database()
+
+    # 配置所需合约的具体参数数据
+    # 合约代码，IF2306 为 股指2306 的合约，仅用于示范，具体合约代码请根据需求自行更改
+    symbol = "IF2306"
+
+    # 交易所，目标合约的交易所
+    exchange = Exchange.CFFEX
+
+    # 历史数据开始时间，精确到日
+    start = datetime(2022, 1, 1)
+
+    # 历史数据结束时间，精确到日
+    end = datetime(2022, 10, 25)
+
+    # 数据的时间粒度，这里示例采用日级别
+    interval = Interval.DAILY
+    # 脚本加载数据库 --- end 1.2 --
+
+    # 数据库读取 --- begin 1.3 --
+    # 读取数据库中k线数据
+    bar1 = database.load_bar_data(
+        symbol=symbol,
+        exchange=exchange,
+        interval=interval,
+        start=start,
+        end=end
+    )
+
+    # 读取数据库中tick数据
+    tick1 = database.load_tick_data(
+        symbol=symbol,
+        exchange=exchange,
+        start=start,
+        end=end
+    )
+
+    # 数据库读取 --- end 1.3 --
+
+    # 数据库写入 --- begin 1.4 --
+    # 需要存入的k线数据，请自行获取并转换成所需的形式
+    # 示例中的bar_data和tick_data均未在示例中展现获取和转换方法
+    # 如需以脚本方式写入，请自行参考源码或其他途径，转换成示例中的数据结构
+    # bar_data: List[BarData] = None
+
+    # database.save_bar_data(bar_data)
+
+    # # 需要存入的k线数据，请自行获取并转换成所需的形式
+    # tick_data: List[TickData] = None
+
+    # # 将tick数据存入数据库
+    # database.save_tick_data(tick_data)
+    # 数据库写入 --- end 1.4 --
+    
 if __name__ == "__main__":
     main()
--- vnpy/trader/setting.py
+++ vnpy/trader/setting.py
@@ -25,17 +25,26 @@ SETTINGS: Dict[str, Any] = {
     "email.sender": "",
     "email.receiver": "",
 
-    "datafeed.name": "",
-    "datafeed.username": "",
-    "datafeed.password": "",
+    # "datafeed.name": "",
+    # "datafeed.username": "",
+    # "datafeed.password": "",
+    "datafeed.name": "tushare",
+    "datafeed.username": "token",
+    "datafeed.password": "c044c2469407484159d4f7dfa2d438f5ac3e4fc89f540264c95ce268",
 
     "database.timezone": get_localzone_name(),
-    "database.name": "sqlite",
-    "database.database": "database.db",
-    "database.host": "",
-    "database.port": 0,
-    "database.user": "",
-    "database.password": ""
+    # "database.name": "sqlite",
+    # "database.database": "database.db",
+    # "database.host": "",
+    # "database.port": 0,
+    # "database.user": "",
+    # "database.password": ""
+    "database.name": "mysql",
+    "database.database": "vnpy",
+    "database.host": "localhost",
+    "database.port": 3306,
+    "database.user": "root",
+    "database.password": "cheng123"
 }
 
 
