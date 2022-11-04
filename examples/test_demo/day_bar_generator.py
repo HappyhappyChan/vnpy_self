14b5504cb2b4b4b7c9869cd9b287258cf49280fd
HappyhappyChan <1214999431@qq.com>
2022-11-04 16:43:29 +0800

    合成57秒K线

--- examples/test_demo/day_bar_generator.py
+++ examples/test_demo/day_bar_generator.py
@@ -1,7 +1,7 @@
 from vnpy.trader.utility import BarGenerator
 from typing import Callable
 from vnpy.trader.constant import Interval
-from vnpy.trader.object import BarData
+from vnpy.trader.object import BarData, TickData
 from datetime import datetime
 class BarGenerator_Daily(BarGenerator):
     """日线合成器"""
@@ -163,5 +163,88 @@ class BarGenerator_Special(BarGenerator):
             self.on_window_bar(self.window_bar)
             self.window_bar = None
             self.min_count_num = 0
-            
+
         self.last_bar = bar
+
+class BarGenerator_MS(BarGenerator):
+    """57s合成K线"""
+    def __init__(
+        self,
+        on_bar: Callable,
+        window: int = 0,
+        on_window_bar: Callable = None,
+        interval: int = Interval.MINUTE
+    ):
+        super().__init__(
+            on_bar,
+            window,
+            on_window_bar,
+            interval
+        )
+    def update_tick(self, tick: TickData) -> None:
+        """
+        Update new tick data into generator.
+        """
+        new_minute: bool = False
+
+        # Filter tick data with 0 last price
+        # 过滤流动性不好的合约
+        if not tick.last_price:
+            return
+
+        # Filter tick data with older timestamp
+        if self.last_tick and tick.datetime < self.last_tick.datetime:
+            return
+
+        # self.bar初始化是None
+        if not self.bar:
+            new_minute = True
+        elif self.last_tick.datetime.second < 57 and tick.datetime.second >= 57:
+            """
+            但实际上这是有问题的 
+            21.00.00-21.00.57, 21.00.58-21.01.56 但是bar的记录时间是以起始时间记录的，就会导致
+            21.00.00有2根K线
+            """
+            self.bar.datetime = self.bar.datetime.replace(
+                second=0, microsecond=0
+            )
+            self.on_bar(self.bar)
+
+            new_minute = True
+
+        if new_minute:
+            self.bar = BarData(
+                symbol=tick.symbol,
+                exchange=tick.exchange,
+                interval=Interval.MINUTE,
+                datetime=tick.datetime,
+                gateway_name=tick.gateway_name,
+                open_price=tick.last_price,
+                high_price=tick.last_price,
+                low_price=tick.last_price,
+                close_price=tick.last_price,
+                open_interest=tick.open_interest
+            )
+        else:
+            self.bar.high_price = max(self.bar.high_price, tick.last_price)
+            if tick.high_price > self.last_tick.high_price:
+                self.bar.high_price = max(self.bar.high_price, tick.high_price)
+
+            self.bar.low_price = min(self.bar.low_price, tick.last_price)
+            if tick.low_price < self.last_tick.low_price:
+                self.bar.low_price = min(self.bar.low_price, tick.low_price)
+
+            self.bar.close_price = tick.last_price
+            # 持仓量
+            self.bar.open_interest = tick.open_interest
+            self.bar.datetime = tick.datetime
+
+        if self.last_tick:
+            # 成交量变化
+            volume_change: float = tick.volume - self.last_tick.volume
+            self.bar.volume += max(volume_change, 0)
+
+            turnover_change: float = tick.turnover - self.last_tick.turnover
+            self.bar.turnover += max(turnover_change, 0)
+
+        self.last_tick = tick   
