ba27c35faa6f03c6ed0a2bad4132f606103389e9
HappyhappyChan <1214999431@qq.com>
2022-11-10 20:46:30 +0800

    add atr_rsi_strategy comments

--- vnpy/strategies/atr_rsi_strategy.py
+++ vnpy/strategies/atr_rsi_strategy.py
@@ -15,12 +15,12 @@ class AtrRsiStrategy(CtaTemplate):
 
     author = "用Python的交易员"
 
-    atr_length = 22
-    atr_ma_length = 10
-    rsi_length = 5
-    rsi_entry = 16
-    trailing_percent = 0.8
-    fixed_size = 1
+    atr_length = 22         # 计算ATR指标的窗口数   
+    atr_ma_length = 10      # 计算ATR均线的窗口数
+    rsi_length = 5          # 计算RSI的窗口数
+    rsi_entry = 16          # RSI的开仓信号
+    trailing_percent = 0.8  # 百分比移动止损
+    fixed_size = 1          # 每次交易的数量
 
     atr_value = 0
     atr_ma = 0
@@ -59,7 +59,7 @@ class AtrRsiStrategy(CtaTemplate):
         Callback when strategy is inited.
         """
         self.write_log("策略初始化")
-
+        # 初始化RSI入场阈值
         self.rsi_buy = 50 + self.rsi_entry
         self.rsi_sell = 50 - self.rsi_entry
 
@@ -88,39 +88,59 @@ class AtrRsiStrategy(CtaTemplate):
         Callback of new bar data update.
         """
         self.cancel_all()
-
+        # 保存K线数据
         am = self.am
         am.update_bar(bar)
         if not am.inited:
             return
-
+        # 计算指标数值
         atr_array = am.atr(self.atr_length, array=True)
         self.atr_value = atr_array[-1]
         self.atr_ma = atr_array[-self.atr_ma_length:].mean()
         self.rsi_value = am.rsi(self.rsi_length)
 
+        # 判断是否要进行交易
+        
+        # 当前无仓位
         if self.pos == 0:
+            # 先初始化K线日高点和日低点的值
             self.intra_trade_high = bar.high_price
             self.intra_trade_low = bar.low_price
 
+            # ATR数值上穿其移动平均线，说明行情短期内波动加大
+            # 即处于趋势的概率较大，适合CTA开仓
             if self.atr_value > self.atr_ma:
+                # 使用RSI指标的趋势行情时，会在超买超卖区钝化特征，作为开仓信号
                 if self.rsi_value > self.rsi_buy:
+                    # 这里为了保证成交，选择超价5个整指数点下单
                     self.buy(bar.close_price + 5, self.fixed_size)
                 elif self.rsi_value < self.rsi_sell:
                     self.short(bar.close_price - 5, self.fixed_size)
-
+        
+        # 持有多头仓位
         elif self.pos > 0:
+            # 计算多头持有期内的最高价，以及重置最低价
+            # 统计当日K线达到的最高点
             self.intra_trade_high = max(self.intra_trade_high, bar.high_price)
             self.intra_trade_low = bar.low_price
 
+            # 计算多头移动止损
+            # 移动止损设置为当K线达到日高点后 \
+            # 回落 trailing_percent 时用停止单（Stop Order）进行平仓离场
             long_stop = self.intra_trade_high * \
                 (1 - self.trailing_percent / 100)
+            
+            # 发出本地止损委托
             self.sell(long_stop, abs(self.pos), stop=True)
 
+        # 持有空头仓位
         elif self.pos < 0:
+            # min（）函数来统计当日K线达到的最底点
             self.intra_trade_low = min(self.intra_trade_low, bar.low_price)
             self.intra_trade_high = bar.high_price
 
+            # 移动止损设置为当K线达到日低点后 \
+            # 反弹0.8%时用停止单（Stop Order）进行平仓离场。
             short_stop = self.intra_trade_low * \
                 (1 + self.trailing_percent / 100)
             self.cover(short_stop, abs(self.pos), stop=True)
